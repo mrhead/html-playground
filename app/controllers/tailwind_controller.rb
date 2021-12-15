21da71fe853c01c089d255f413f1eb684f4770ba
Patrik Bóna <patrik.bona@mrhead.sk>
2021-12-15 21:56:28 +0100

    wip - do not merge

--- app/controllers/tailwind_controller.rb
+++ app/controllers/tailwind_controller.rb
@@ -1,17 +1,6 @@
 class TailwindController < ApplicationController
   def blog_post
-    @post = OpenStruct.new(
-      title: Faker::Lorem.sentence(supplemental: true),
-      content: 5.times.map do
-        Faker::Lorem.paragraph(
-          sentence_count: 10,
-          random_sentences_to_add: 10,
-          supplemental: true,
-        )
-      end.join("\n\n"),
-      author: "John Doe",
-      published_at: Time.current
-    )
+    @post = Post.generate
   end
 
   def sign_in
