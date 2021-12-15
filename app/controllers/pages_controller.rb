89059b0520510f994224e0881f8c8380e2452906
Patrik Bóna <patrik.bona@mrhead.sk>
2021-12-15 20:38:00 +0100

    Add pages index

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+    @pages = get_pages
+  end
+
+  private
+
+  def get_pages
+    TailwindController.action_methods.map do |action|
+      {
+        name: action.humanize,
+        path: url_for(controller: :tailwind, action: action, only_path: true)
+      }
+    end
+  end
+end
